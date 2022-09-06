d6d4d1d9dc3b667b77e670cba1341472b62aaaac
Jesus Monge <jesus_jaml@outlook.com>
2022-09-05 22:23:44 -0500

    rn elements

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,10 +1,12 @@
 import React, {useRef} from 'react';
-import {StyleSheet, View, Animated} from 'react-native';
+import {StyleSheet, View, Animated, Button, Text, Easing} from 'react-native';
 
 import {colors} from '../constants/colors';
+import { useAnimation } from '../hooks/useAnimation';
 
 export const Animation101Screen = () => {
-  const opacity = useRef(new Animated.Value(0.5)).current;
+
+  const { fadeIn, fadeOut, opacity, position, startMovingPosition } = useAnimation()
 
   return (
     <View style={styles.container}>
@@ -12,8 +14,25 @@ export const Animation101Screen = () => {
         style={{
           ...styles.box,
           opacity,
+          transform: [
+            {
+              translateX: position,
+            },
+          ],
         }}
       />
+
+      <Button 
+        title="FadeIn" 
+        onPress={ () => {
+          fadeIn()
+          startMovingPosition( 150, 600 )
+        }} 
+      />
+
+      <Text />
+
+      <Button title="FadeOut" onPress={fadeOut} />
     </View>
   );
 };
@@ -25,7 +44,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box: {
-    width: 175,
+    marginBottom: 20,
+    width: 150,
     aspectRatio: 1,
     backgroundColor: colors.primary,
   },
--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import { colors } from '../constants/colors';
+import React, {useRef} from 'react';
+import {Animated, PanResponder, StyleSheet, View} from 'react-native';
+import {colors} from '../constants/colors';
 
 export const Animation102Screen = () => {
-    return (
-        <View style={ styles.container }>
-            <View style={ styles.box }/>
-        </View>
-    );
-}
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+        [
+      null,
+      { dx: pan.x, dy: pan.y, },
+    ],{ 
+        useNativeDriver: false 
+    }),
+    onPanResponderRelease: () => {
+      Animated.spring(pan, {toValue: {x: 0, y: 0}, useNativeDriver: false}).start();
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View 
+        {...panResponder.panHandlers}
+        style={[
+            pan.getLayout(),
+            styles.box
+        ]} 
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    box: {
-        width: 175,
-        aspectRatio: 1,
-        backgroundColor: colors.secondary
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 175,
+    aspectRatio: 1,
+    backgroundColor: colors.secondary,
+    borderRadius: 12,
+  },
 });
